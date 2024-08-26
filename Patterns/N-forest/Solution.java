d4c84d1d792ae5a22ef2ae53a08ea272705a3138
avantika-14 <100148210+avantika-14@users.noreply.github.com>
2024-08-26 19:26:00 +0530

    Create Solution.java

---
+++ Patterns/N-forest/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public static void nForest(int n) {
+        // Write your code here
+        int i,j;
+        for(i=1;i<=n;i++){
+            for(j=1;j<=n;j++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
