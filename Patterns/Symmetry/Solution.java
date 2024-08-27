95365ee1bc9450642cd50ad0165b674576e61223
avantika-14 <100148210+avantika-14@users.noreply.github.com>
2024-08-27 09:21:51 +0530

    Create Solution.java

---
+++ Patterns/Symmetry/Solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public static void symmetry(int n) {
+        // Write your code here
+         n = 2*n;
+        for(int row = 0; row < n-1; row++) {
+            for(int col = 0; col < n; col++) {
+                // Condition for first half of the rows.
+                if (row < n/2 && (col <= row || col >= (n-row-1))) {
+                    System.out.print("*");
+                }
+                // Condition for the second half of the rows.
+                else if (row >= n/2 && (col < (n-row-1) || col > row)) {
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+
+                System.out.print(" ");
+            }
+
+            // End the current row of the pattern.
+            System.out.println();
+        }
+    }
+}
