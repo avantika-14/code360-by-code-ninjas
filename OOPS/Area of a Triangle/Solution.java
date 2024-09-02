8e866a063b28573ddca7ecccd3f5d255ecad9dae
avantika-14 <100148210+avantika-14@users.noreply.github.com>
2024-09-02 13:50:56 +0530

    Create Solution.java

---
+++ OOPS/Area of a Triangle/Solution.java
@@ -0,0 +1,10 @@
+import java.util.* ;
+import java.io.*; 
+public class Rectangle { 
+    // Write your code here.
+    int length;
+    int breadth;
+    public int getArea(){
+        return length * breadth;
+    }
+}
